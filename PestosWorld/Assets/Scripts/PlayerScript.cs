c6b456e46f8536a36819ec4b7ab0299811c64645
EnzoGuillouche <145991192+EnzoGuillouche@users.noreply.github.com>
2025-08-11 16:33:13 +0200

    am a DESIGNER

--- PestosWorld/Assets/Scripts/PlayerScript.cs
+++ PestosWorld/Assets/Scripts/PlayerScript.cs
@@ -9,7 +9,7 @@ public class PlayerScript : MonoBehaviour
     private InputAction moveAction;
     private Vector2 moveInput;
     private readonly float speed = 7f;
-    private int direction = 0; // 0 = front, 1 = left, 2 = right, 3 = back
+    private Vector2 direction; //  (0 = left, 1 = right), (0 = front, 1 = back)
 
     void Start()
     {
@@ -22,25 +22,39 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         moveInput = moveAction.ReadValue<Vector2>();
+
         if (moveInput.x != 0)
         {
             animator.SetBool("isMovingX", true);
             if (moveInput.x < 0)
             {
-                if (direction != 1)
+                if (direction.x != 1)
                     Flip(-1, 1);
-                direction = 1;
+                direction.x = 1;
             }
-            if (moveInput.x > 0)
+            else
             {
-                if (direction != 2)
+                if (direction.x != 0)
                     Flip(-1, 1);
-                direction = 2;
+                direction.x = 0;
             }
         }
         else
             animator.SetBool("isMovingX", false);
-        // if (moveInput.y != 0)
+        
+        if (moveInput.y != 0)
+        {
+            animator.SetBool("isMovingY", true);
+            if (moveInput.y < 0)
+            {
+                direction.y = 1;
+            }
+            else 
+                direction.y = 0;
+        }
+        else
+            animator.SetBool("isMovingY", false);
+
         rb.linearVelocity = new Vector2(moveInput.x * speed, moveInput.y * speed);
     }
 
